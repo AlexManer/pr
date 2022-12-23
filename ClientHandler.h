d94ffef86e0b5f44632793c688623078c038cb79
AlexManer <119928789+AlexManer@users.noreply.github.com>
2022-12-23 18:32:02 +0700

    Add files via upload

---
+++ ClientHandler.h
@@ -0,0 +1,90 @@
+#ifndef LAB32_CLIENTHANDLER_H
+#define LAB32_CLIENTHANDLER_H
+
+
+#include <string>
+#include <iostream>
+#include <sys/poll.h>
+#include <cstring>
+#include <algorithm>
+#include "Proxy.h"
+#include "CacheRecord.h"
+#include "Cache.h"
+
+#define timeOut 1000
+#define my_delete(x) {delete x; x = NULL;}
+
+namespace lab32 {
+
+	class ClientHandler {
+	private:
+		std::string request;
+		std::string prVersion;
+		int clientSocket;
+		std::string lastField;
+		std::string url;
+		std::string host;
+		std::string port = "80";
+		std::string errorMsg;
+		std::string headers;
+		bool isConnected = false;
+		pthread_t serverThread;
+		Cache* cache;
+		CacheRecord* record;
+		size_t readPointer = 0;
+		bool initialized = false;
+		int serverSocket;
+		static bool
+			createServer(ClientHandler* client, CacheRecord* record, const std::string& host, const std::string& request);
+		static int connectToServer(ClientHandler* client);
+
+	public:
+
+		void deleteEvent(pollfd* conn, short event);
+
+		void setCache(Cache* cache);
+
+		void setServerThread(pthread_t serverThread);
+
+		explicit ClientHandler(int socket);
+
+		bool readFromCache(pollfd* connection);
+
+		const std::string& getErrorMsg() const { return errorMsg; };
+
+		int getSocket() const { return clientSocket; };
+
+		bool receive(ClientHandler* client);
+
+		static void* clientHandlerRoutine(void* args);
+
+		static std::string getPrVersion(std::string in);
+
+		static std::string getUrl(std::string in);
+
+		static std::string getMethod(std::string in);
+
+		bool RequestParser();
+
+		bool sendRequest(const std::string& request) const;
+
+		short returnRead(pollfd* conn);
+
+		bool isOneLineRequest();
+
+		void buildRequest(std::string& HTTPMethod, std::string& serverMethodPath);
+
+		std::string getUrl(std::string host, std::string serverMethodPath);
+
+		std::string getServerMethodPath(std::string in, std::string HTTPMethod);
+
+		std::string getHost(std::string in);
+
+		size_t findFirstSpChar(std::string in);
+
+		void changeRequestMethodPath(std::string host);
+	};
+
+}
+
+#endif //LAB32_CLIENTHANDLER_H#pragma once
---
+++ ServerHandler.h
@@ -0,0 +1,39 @@
+#ifndef LAB32_SERVERHANDLER_H
+#define LAB32_SERVERHANDLER_H
+
+#include <iostream>
+#include <sys/poll.h>
+#include "../Cache/CacheRecord.h"
+
+#define my_delete(x) {delete x; x = NULL;}
+#define timeOut 1000
+
+namespace lab32 {
+
+	class ServerHandler {
+	private:
+		int serverSocket;
+		pollfd connection;
+		CacheRecord* cacheRecord;
+
+	public:
+
+		static void* serverHandlerRoutine(void* args);
+
+		ServerHandler(int socket, CacheRecord* record);
+
+		int getSocket() const { return serverSocket; };
+
+		CacheRecord* getCacheRecord() { return cacheRecord; };
+
+		void setRecordInvalid() { cacheRecord->setBroken(); };
+
+		~ServerHandler();
+
+		bool receive();
+
+		void addNewConnection(short event);
+	};
+}
+
+#endif //LAB32_SERVERHANDLER_H
